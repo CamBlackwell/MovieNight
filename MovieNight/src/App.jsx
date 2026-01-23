c94823792b10cbb3ed6beba810322a9ebfe583a2
CamBlackwell <cameronBlackwell89@gmail.com>
2026-01-23 12:23:37 +1030

    feat: added banner to top

--- MovieNight/src/App.jsx
+++ MovieNight/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 import Home from "./pages/Home";
 import Message from "./pages/Messages";
@@ -9,18 +10,15 @@ import NotFound from "./pages/NotFound";
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col bg-gray-900 text-white">
-
-        {/* NAVIGATION */}
-        <nav className="w-full bg-white text-black shadow px-6 py-4 flex items-center gap-6">
-          <Link to="/" className="font-semibold text-lg">Home</Link>
-          <Link to="/messages" className="hover:underline">Messages</Link>
-          <Link to="/alert" className="hover:underline">Weekly Alert</Link>
-          <Link to="/history" className="hover:underline">History</Link>
+      <div className="app-container">
+        <nav className="nav-bar">
+          <Link to="/" className="nav-link nav-home">Home</Link>
+          <Link to="/messages" className="nav-link">Messages</Link>
+          <Link to="/alert" className="nav-link">Weekly Alert</Link>
+          <Link to="/history" className="nav-link">History</Link>
         </nav>
 
-        {/* PAGE CONTENT */}
-        <main className="flex-1 p-6">
+        <main className="page-content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/messages" element={<Message />} />
@@ -29,7 +27,6 @@ export default function App() {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-
       </div>
     </Router>
   );
